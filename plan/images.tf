4b9fd6ebf96368858e0e45f348a3149b37fe8d86
Mohyaddin Saleh <mo7y.66@gmail.com>
2023-02-08 21:27:34 +0300

    Ensured the plan functionality regardless of the docker provider updates.

--- plan/images.tf
+++ plan/images.tf
@@ -1,7 +1,7 @@
 resource "docker_image" "os" {
   name = "os"
   build {
-    path = "build"
+    context = "build"
     dockerfile = "os.Dockerfile"
     tag = ["kernel:latest"]
   }
@@ -11,7 +11,7 @@ resource "docker_image" "php" {
   depends_on = [docker_image.os]
   name = "php"
   build {
-    path = "build"
+    context = "build"
     dockerfile = "php.Dockerfile"
     tag = ["php:8"]
   }
@@ -21,7 +21,7 @@ resource "docker_image" "nginx" {
   depends_on = [docker_image.os]
   name = "nginx"
   build {
-    path = "build"
+    context = "build"
     dockerfile = "nginx.Dockerfile"
     tag = ["nginx:latest"]
   }
@@ -31,7 +31,7 @@ resource "docker_image" "node" {
   depends_on = [docker_image.os]
   name = "node"
   build {
-    path = "build"
+    context = "build"
     dockerfile = "node.Dockerfile"
     tag = ["node:14"]
   }
--- plan/proiders.tf
+++ plan/proiders.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
+      version = "3.0.1"
     }
   }
 }
